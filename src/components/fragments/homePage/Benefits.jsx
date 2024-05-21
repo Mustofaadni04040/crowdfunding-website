4eb956e91ed0a3a6fc8279b405b61ded070c4529
mustofa adny <adniadni04@gmail.com>
2024-05-21 23:20:35 +0700

    add benefits section

---
+++ src/components/fragments/homePage/Benefits.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { PiHandshake } from 'react-icons/pi';
+import { LuHeartHandshake } from 'react-icons/lu';
+import { LiaHandHoldingUsdSolid } from 'react-icons/lia';
+
+export default function Benefits() {
+  return (
+    <section className="w-full border border-y-primary p-5 my-7">
+      <div className="container mx-auto">
+        <h1 className="text-2xl text-primary font-bold lg:text-4xl">
+          MENGAPA BERBAGI
+          <br /> BERSAMA KAMI?
+        </h1>
+        <div className="flex flex-col lg:flex-row lg:px-20">
+          <div className="flex items-center gap-3">
+            <PiHandshake className="text-primary text-9xl" />
+            <div>
+              <p className="text-primary text-lg font-bold lg:mb-3">
+                Responsif
+              </p>
+              <p className="text-sm text-primary">
+                Merespon kebutuhan masyarakat palestina dengan cepat dan tepat
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <LuHeartHandshake className="text-primary text-9xl" />
+            <div>
+              <p className="text-primary text-lg font-bold lg:mb-3">
+                Sustainable Program
+              </p>
+              <p className="text-sm text-primary">
+                Program jangka panjang untuk kemandirian masyarakat palestina
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-3">
+            <LiaHandHoldingUsdSolid className="text-primary text-9xl" />
+            <div>
+              <p className="text-primary text-lg font-bold lg:mb-3">
+                Credibility
+              </p>
+              <p className="text-sm text-primary">
+                Bertanggung jawab penuh menjalankan amanah program
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroList from '../components/fragments/homePage/HeroList';
 import Milestones from '../components/fragments/homePage/Milestones';
+import Benefits from '../components/fragments/homePage/Benefits';
 
 export default function HomePage() {
   return (
     <>
       <HeroList />
       <Milestones />
+      <Benefits />
     </>
   );
 }
