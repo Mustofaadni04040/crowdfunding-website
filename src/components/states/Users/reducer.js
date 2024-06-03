51322fd5a235a955be66a55290b111f16c01dad0
SideeID <sandipratama164026@gmail.com>
2024-06-03 22:58:15 +0700

    feat: Add CRUD actions and reducer for Users component

---
+++ src/components/states/Users/reducer.js
@@ -0,0 +1,37 @@
+const initialState = {
+  users: [],
+  user: {},
+  totalPages: 1,
+  currentPage: 1,
+};
+
+const usersReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case 'GET_USERS':
+      return {
+        ...state,
+        users: action.payload.users,
+        totalPages: action.payload.totalPages,
+        currentPage: action.payload.currentPage,
+      };
+    case 'GET_USER':
+      return { ...state, user: action.payload };
+    case 'CREATE_USER':
+      return { ...state, users: [...state.users, action.payload] };
+    case 'UPDATE_USER':
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user._id === action.payload._id ? action.payload : user),
+      };
+    case 'DELETE_USER':
+      return {
+        ...state,
+        users: state.users.filter((user) => user._id !== action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
