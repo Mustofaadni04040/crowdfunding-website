10d2cf92aa47df06d2a058b254692787047c9aab
SideeID <sandipratama164026@gmail.com>
2024-06-07 10:03:25 +0700

    feat: Add ToastContainer for displaying toast notifications

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Slide, ToastContainer } from 'react-toastify';
 import App from './App';
 import './index.css';
 import store from './components/states';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer
+        autoClose={2000}
+        position="top-right"
+        transition={Slide}
+        closeOnClick
+      />
     </Provider>
   </React.StrictMode>,
 );
