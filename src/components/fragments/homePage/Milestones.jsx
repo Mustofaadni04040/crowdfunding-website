eca0e87e6309ce9466a9d09e2d0b3c985cf980dd
SideeID <sandipratama164026@gmail.com>
2024-06-10 18:09:40 +0700

    chore: Update Milestones component to fix useSelector selector

--- src/components/fragments/homePage/Milestones.jsx
+++ src/components/fragments/homePage/Milestones.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import formattedTotal from '../../../utils/FormattedTotal';
 
 export default function Milestones() {
-  const fundraisers = useSelector((state) => state.fundraisers);
+  const fundraisers = useSelector((state) => state.fundraisers.fundraisers);
 
   const totalCollectedDonations = fundraisers.reduce(
     (total, fundraiser) => total + fundraiser.collectedAmount,
@@ -18,19 +18,19 @@ export default function Milestones() {
   return (
     <section className="container px-5 mx-auto">
       <div className="flex flex-col mx-auto text-center lg:flex-row lg:max-w-[700px]">
-        <div className="border-b mx-auto w-full border-slate-300 bg-primary py-2 rounded-tr-lg rounded-tl-lg lg:rounded-tr-none lg:rounded-bl-lg lg:border-b-0 lg:border-r lg:py-5">
+        <div className="w-full py-2 mx-auto border-b rounded-tl-lg rounded-tr-lg border-slate-300 bg-primary lg:rounded-tr-none lg:rounded-bl-lg lg:border-b-0 lg:border-r lg:py-5">
           <p className="text-sm text-white lg:text-lg">Program Donasi</p>
           <p className="text-sm font-bold text-white lg:text-xl">
             {fundraisers.length}
           </p>
         </div>
-        <div className="border-b mx-auto w-full border-slate-300 bg-primary py-2 lg:border-b-0 lg:border-r lg:py-5">
+        <div className="w-full py-2 mx-auto border-b border-slate-300 bg-primary lg:border-b-0 lg:border-r lg:py-5">
           <p className="text-sm text-white lg:text-lg">Dana Terkumpul</p>
           <p className="text-sm font-bold text-white lg:text-xl">
             {formattedTotal(totalCollectedDonations)}
           </p>
         </div>
-        <div className="mx-auto w-full bg-primary py-2 rounded-br-lg rounded-bl-lg lg:rounded-bl-none lg:rounded-tr-lg lg:py-5">
+        <div className="w-full py-2 mx-auto rounded-bl-lg rounded-br-lg bg-primary lg:rounded-bl-none lg:rounded-tr-lg lg:py-5">
           <p className="text-sm text-white lg:text-lg">Donatur Terdaftar</p>
           <p className="text-sm font-bold text-white lg:text-xl">
             {donationsTotal}
