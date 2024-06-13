f622d7926fe12dc3f72f7c9f3109b0450d2faf8c
SideeID <sandipratama164026@gmail.com>
2024-06-13 09:03:26 +0700

    chore: Update api.js to use production backend URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:6005',
+  baseURL: 'https://crowdfunding-backend-drab.vercel.app',
 });
 
 api.interceptors.request.use(
