3e558ad529ca842863174b1f57678d19ed4b1c50
SideeID <sandipratama164026@gmail.com>
2024-06-13 09:02:06 +0700

    chore: Update api.js to use local development server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://crowdfunding-backend-drab.vercel.app',
-  withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'http://localhost:6005',
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
 export default api;
